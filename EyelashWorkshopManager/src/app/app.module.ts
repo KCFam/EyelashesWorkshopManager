097dc3ff46eacf037e29d6245ffdf86e91b64fca
Duong <pduong@deloitte.com>
2019-10-22 17:26:31 -0400

    Added navbar

--- EyelashWorkshopManager/src/app/app.module.ts
+++ EyelashWorkshopManager/src/app/app.module.ts
@@ -9,23 +9,40 @@ import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 // material angular
 import { MatButtonModule } from "@angular/material/button";
-import { LayoutHeaderComponent } from './_layout/layout-header/layout-header.component';
-import { LayoutMainComponent } from './_layout/layout-main/layout-main.component';
-import { LayoutFooterComponent } from './_layout/layout-footer/layout-footer.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { LoginComponent } from './login/login.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+
+// Pages
+import { LayoutHeaderComponent } from "./_layout/layout-header/layout-header.component";
+import { LayoutMainComponent } from "./_layout/layout-main/layout-main.component";
+import { LayoutFooterComponent } from "./_layout/layout-footer/layout-footer.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { LoginComponent } from "./login/login.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
-  declarations: [AppComponent, LayoutHeaderComponent, LayoutMainComponent, LayoutFooterComponent, DashboardComponent, LoginComponent],
+  declarations: [
+    AppComponent,
+    LayoutHeaderComponent,
+    LayoutMainComponent,
+    LayoutFooterComponent,
+    DashboardComponent,
+    LoginComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     // ng-bootstrap
     NgbModule,
     // material angular
     MatButtonModule,
-    BrowserAnimationsModule
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
